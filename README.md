76fc526dbd4dd5a6f46a9e63677114161ed7ab42
Usman Awan <usman@norbay.no>
2024-06-29 02:59:58 +0200

    NOJIRA: Created project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Spring Security Tests
+
+I am trying to learn and gather examples of how we can test security in SpringBoot apps.
+
+### Example tests
+
+* Unit testing
+* Integration testing
+
+### Local env
+
+* OpenJDK 21
+* Maven
---
+++ src/test/java/no/norbay/springbootsecuritytests/SecurityApplicationTest.java
@@ -0,0 +1,13 @@
+package no.norbay.springbootsecuritytests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
